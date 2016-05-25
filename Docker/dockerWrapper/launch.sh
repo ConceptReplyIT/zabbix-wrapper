31024d556899dd910b6b485f199ee87d6634eac0
Michele Grandolfo <m.grandolfo@reply.it>
2016-05-25 18:30:06 +0200

    Modified launch.sh for enabling debug from remote

--- Docker/dockerWrapper/launch.sh
+++ Docker/dockerWrapper/launch.sh
@@ -5,5 +5,11 @@ ZONE_CONFIG_FILE="$JBOSS_HOME/standalone/deployments/$WAR_NAME.war/WEB-INF/class
 sed -i "s/^\( *url: \).*$/\1$(echo $ZABBIX_URL | sed -e 's/[\/&]/\\&/g')/" ${ZONE_CONFIG_FILE};
 sed -i "s/^\( *username: \).*$/\1$(echo $ZABBIX_USERNAME | sed -e 's/[\/&]/\\&/g')/" ${ZONE_CONFIG_FILE};
 sed -i "s/^\( *password: \).*$/\1$(echo $ZABBIX_PASSWORD | sed -e 's/[\/&]/\\&/g')/" ${ZONE_CONFIG_FILE};
+
+if [ "${ENABLE_DEBUG}" = "true" ];
+	then DEBUG_ARG="--debug";
+	else DEBUG_ARG="";
+fi
+
 CLUSTER_MESSAGING_PASSWORD="pwd"
 $JBOSS_HOME/bin/standalone.sh -c $JBOSS_CONF_FILE -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.management=0.0.0.0
